933a29bc789b66139d2b455f921165549dd02e45
jinglei <jinglei@qq.com>
2022-09-09 16:35:55 +0800

    v0.1.0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ export default class RNBaseui extends React.Component{
     render() {
         return (
             <Text>
-                111123
+                1123
             </Text>
         );
     }
